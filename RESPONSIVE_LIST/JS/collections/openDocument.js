af2c43009d65ffab461d2e75f6c35292c1c35c24
Charles Bonnissent <charles.bonnissent@anakeen.com>
2015-03-16 14:14:28 +0100

    feat(openDocs): select next one when closed is selected

--- RESPONSIVE_LIST/JS/collections/openDocument.js
+++ RESPONSIVE_LIST/JS/collections/openDocument.js
@@ -11,12 +11,22 @@ define([
 
         initialize : function odc__initialize() {
             this.listenTo(this, "selected", this.propagateSelected);
+            this.listenTo(this, "destroy", this.selectAnotherOne);
         },
 
         propagateSelected : function odc__selected(model) {
             this.each(function odc__isSelected(currentModel) {
                 currentModel.set("selected", model.id === currentModel.id);
             });
+        },
+
+        selectAnotherOne : function odc_selectAnotherOne(model, collection, options) {
+            var newModel = this.at(options.index) || this.at(options.index - 1);
+            if (model.get("selected")) {
+                if (newModel) {
+                    newModel.trigger("selected", newModel);
+                }
+            }
         }
     });
 });
